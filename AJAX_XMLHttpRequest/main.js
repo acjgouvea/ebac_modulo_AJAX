4a82b3373a4d5931ec77efec7df2b0aa61c51128
carlos gouvea <108358794+acjgouvea@users.noreply.github.com>
2024-03-11 13:00:31 -0300

    commit

--- AJAX_XMLHttpRequest/main.js
+++ AJAX_XMLHttpRequest/main.js
@@ -1,39 +1,40 @@
-/*
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('btn-buscar-cep').addEventListener('click', function () {
-        const xhttp = new XMLHttpRequest();
-        const cep = document.getElementById('cep').value; 
-        const endpoint = `https://viacep.com.br/ws/${cep}/json`; 
-
-        xhttp.open('GET', endpoint);
-        xhttp.send();
-    });
-});
-
-*/
-
 $(document).ready(function () {
     $('#btn-buscar-cep').click(function () {
         const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
         const botao = $(this);
-        $(botao).find('i').addClass('d-none');
-        $(botao).find('span').removeClass('d-none');
+        botao.find('i').addClass('d-none');
+        botao.find('span').removeClass('d-none');
 
-        $.ajax(endpoint).done(function (resposta) {
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.localidade;
-            const estado = resposta.uf;
-            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-            $('#endereco').val(endereco);
-
-
-            setTimeout(function () {
-                $(botao).find('i').removeClass('d-none');
-                $(botao).find('span').addClass('d-none');
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(json => {
+                const { logradouro, bairro, localidade, uf } = json;
+                const endereco = `${logradouro}, ${bairro} - ${localidade} - ${uf}`;
             })
-        },4000);
+            .catch(error => {
+                console.error('Ocorreu um erro:', error);
+                alert("Ocorreu um erro ao buscar o CEP. Por favor, tente novamente.");
+            })
+            .finally(() => {
+                setTimeout(() => {
+                    botao.find('i').removeClass('d-none');
+                    botao.find('span').addClass('d-none');
+                }, 4000);
+            });
+    });
+
+    $('#formulario-pedido').submit(function (evento) {
+        evento.preventDefault();
+
+        const nome = $('#nome').val().trim();
+        if (nome.length === 0) {
+            alert('Por favor, digite o nome.');
+
+            throw new Error('Por favor, digite o nome.');
+            return;
+        }
+
+
     });
 });
